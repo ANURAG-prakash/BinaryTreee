e65c2055a9e036cacdb6f2b262749b47d7d4ff85
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-31 06:45:23 +0530

    UC1 has Added

---
+++ BinarySearchTree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTreeDemo
+{
+    class BinarySearchTree<T> where T: IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> leftTree { get; set; }
+        public BinarySearchTree<T> rightTree { get; set; }
+
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.rightTree = null;
+            this.leftTree = null;
+
+        }
+
+        int leftCount = 0, rightCount = 0;
+        bool resul = false;
+        public void Insert(T item)
+        {
+            T CurrentNodeValue = this.NodeData;
+            if((CurrentNodeValue.CompareTo(item))>0)
+            {
+                if (this.leftTree == null)
+                    this.leftTree = new BinarySearchTree<T>(item);
+                else
+                    this.leftTree.Insert(item);
+            }
+            else
+            {
+                if (this.rightTree == null)
+                    this.rightTree = new BinarySearchTree<T>(item);
+                else
+                    this.rightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if(this.rightTree != null)
+            {
+                this.rightCount++;
+                this.rightTree.Display();
+            }
+
+        }
+       
+    }
+}
